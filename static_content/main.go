cd19d1aef0ead976df35c48abddaf57762b609a9
David Anderson <dave@natulte.net>
2014-04-12 16:36:18 -0700

    Allow serving multiple roots.

--- static_content/main.go
+++ static_content/main.go
@@ -5,11 +5,12 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 )
 
 var (
-	root          = flag.String("root", "", "Directory to serve")
+	roots         = flag.String("roots", "", "Directories to serve")
 	listenHttps   = flag.String("listen-https", ":443", "Address to listen on for HTTPS")
 	listenHttp    = flag.String("listen-http", ":80", "Address to listen on for HTTP")
 	certDir       = flag.String("certdir", "", "Directory containing certs and keys")
@@ -20,12 +21,20 @@ var (
 func main() {
 	flag.Parse()
 
-	if *root == "" {
+	if *roots == "" {
 		log.Fatalln("Please specify -root")
 	}
 
+	splitRoots := strings.Split(*roots, ",")
+	for _, root := range splitRoots {
+		path := strings.Split(root, ":")
+		if len(path) != 2 {
+			log.Fatalf("Bad root spec: %s", root)
+		}
+		http.Handle(path[0]+"/", http.FileServer(http.Dir(path[1])))
+	}
+
 	s := &tls_server.Server{
-		Handler:      http.FileServer(http.Dir(*root)),
 		ReadTimeout:  *socketTimeout,
 		WriteTimeout: *socketTimeout,
 		HSTSDuration: *hstsDuration,
